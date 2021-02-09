d41fce3b6d9de0e38a977a796c10d1fdeaa7570b
sannnann131 <tetsu09221991@gmaill.com>
2021-02-09 21:33:03 +0900

    デプロイテスト

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,16 +1,7 @@
 <?php
-function dbConnect(){
-  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['dbname'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $user = $db['user'];
-  $password = $db['pass'];
-  $options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-  );
-  $dbh = new PDO($dsn,$user,$password,$options);
-  return $dbh;
-}
+  $dsn='mysql:dbname=getenv('DB_NAME');host=getenv('DB_HOSTNAME');charset=utf8';
+  $user=getenv('DB_USERNAME');
+  $password=getenv('DB_PASSWORD');
+  $dbh=new PDO($dsn, $user, $password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 ?>

0dc59ee7895c93c9cee6d514b452e302cd8b3912
sannnann131 <tetsu09221991@gmaill.com>
2021-03-03 21:57:15 +0900

    Revert "デプロイテスト"
    This reverts commit e19f9953984a1d9f95b4de8f433c53593c78d7c8.

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,9 +1,7 @@
 <?php
-  $dbname=getenv('DB_NAME');
-  $dbhost=getenv('DB_HOSTNAME');
+  $dsn='mysql:dbname=heroku_55204781f5f56d5;host=us-cdbr-east-03.cleardb.com;charset=utf8';
   $user=getenv('DB_USERNAME');
   $password=getenv('DB_PASSWORD');
-  $dsn='mysql:dbname={$dbname};host={$dbhost};charset=utf8';
   $dbh=new PDO($dsn, $user, $password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 ?>
